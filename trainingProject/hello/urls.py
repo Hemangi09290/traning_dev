310dde792f3a90db1376460b752b4b56756147ea
unknown <barvehemangi09@gmail.com>
2022-04-18 11:59:00 +0530

    updated view functions commit

--- trainingProject/hello/urls.py
+++ trainingProject/hello/urls.py
@@ -4,4 +4,5 @@ from hello import views
 #This url will route to home page through views
 urlpatterns = [
     path("", views.home, name="home"),
+    path("hello/<name>", views.getData, name="getData"),
 ]
--- trainingProject/hello/views.py
+++ trainingProject/hello/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+import re
 
 # Create your views here.
 # This is a home page where Hello django msg will appear
 def home(request):
     return HttpResponse("Hello, Django!!!")
+
+
+#Simple Python Program to see if a given url input name is ascii value or alphanumeric
+# accordingly print message
+# for this you need to update url with /hello/name as this is practise page without html
+def getData(request, name):
+
+    # getting user’s name     
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + name
+    return HttpResponse(content)
