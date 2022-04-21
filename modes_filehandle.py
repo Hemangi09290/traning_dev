112f587d2d93b38bee51ebf0827130a40b1af14d
Hemangi09290 <103880910+Hemangi09290@users.noreply.github.com>
2022-04-21 15:52:42 +0530

    Learn Python file i/o handling

--- modes_filehandle.py
+++ modes_filehandle.py
@@ -13,4 +13,40 @@ def extracting_zipfile():
         zip.extractall()                    
     print("Finished extracting")
     
+def rights_tofile():
+    os.getcwd()
+    print(os.listdir())
+    txt = open('sample2.txt')
+    "To read in binary mode"
+    txt=open('Demo.txt','r+b') 
+    txt=open('Demo.txt','a')
+    txt.close()
+    
+"To read the contents of python file"
+def read_pyfile():
+    with open('Demo.txt') as todo:
+        todo.read()
+
+"To read specific lines from python file"
+def read_specificpylines():
+    with open('Demo.txt') as todo:
+        todo.read(5)
+    
+"tell() will tell us where the cursor is rightnow and it gives int values"
+def tell_pyfile():
+    with open('Demo.txt') as todo:
+        todo.read(5)
+        todo.tell()
+
+"seek() takes an int argument and sets the cursor at the perticular position"
+def seek_pyfile():
+    with open('Demo.txt') as todo:
+        todo.seek(0)
+        todo.read()
+
 extracting_zipfile()
+rights_tofile()
+read_pyfile()
+read_specificpylines()
+tell_pyfile()
+seek_pyfile()
